74fb22016c7e9fb810495bead1e5c0387b6a5b64
sir-shahin <shahin.n14001995@gmail.com>
2026-05-25 16:24:27 +0330

    feat: fixed type eror

--- src/app/(dashboard)/grouplancing/page.tsx
+++ src/app/(dashboard)/grouplancing/page.tsx
@@ -153,7 +153,12 @@ export default function Page() {
                 <CircularProgress color="primary" />
               </Box>
             )}
-            {pricesDataTable?.platforms.map((data: any) => (
+            {pricesDataTable?.platforms.map((data: {
+              platform_uuid:string;
+              platform_name:string;
+              percentage:string;
+              amount:string;
+            }) => (
               <div className="cmp" key={data.platform_uuid}>
                 <div className="cmp-label">{data.platform_name}</div>
                 <div className="cmp-bar-wrap">
