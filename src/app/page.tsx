a7bebb96546049ba552b9db5590e1d4a006d759c
sir-shahin <shahin.n14001995@gmail.com>
2026-05-29 12:45:46 +0330

    feat: back to login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,123 +1,116 @@
 "use client";
-import { Box, Button, Paper, TextField, Typography } from "@mui/material";
-import Image from "next/image";
-import Logo from "@/assets/logo.jpg";
+import React, { useState } from "react";
+import { Box, Button, TextField, Typography } from "@mui/material";
 import { useRouter } from "next/navigation";
-import { useMutation, useQuery } from "@tanstack/react-query";
 import { axiosInstance } from "@/utils/axios";
-import { useState } from "react";
+import Logo from "@/assets/logo.jpg";
+import Image from "next/image";
 
-export default function Login() {
+export default function LoginPages() {
   const router = useRouter();
-  const [mobile, setMobile] = useState("");
-  const [code, setCode] = useState("");
-  const [step, setStep] = useState(0);
-  const [loading, setLoading] = useState(false);
 
-  const handleVerify = () => {
-    setLoading(true);
-    otpRequestMutation.mutateAsync({ identity: mobile });
-  };
-
-  const otpRequestMutation = useMutation({
-    mutationFn: (variables:{ identity: string; }) => axiosInstance.post(`https://etekanesh.com/account/api/otp/send/`, variables),
-    onError: (error) => alert(error),
-    onSettled: () => setLoading(false),
-    onSuccess: () => setStep(1),
+  const [formData, setFormData] = useState({
+    identity: "",
+    otp: "",
   });
+  const [otpSent, setOtpSent] = useState(false);
 
-  const fetchUser = async () => {
-    setLoading(true);
-    const { data } = await axiosInstance.get(`https://etekanesh.com/api/account/detail/`);
-    setLoading(false);
-    return data;
+  const handleChange = (e: any) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
   };
 
-  // Use the tanstack composable
-  const { refetch } = useQuery({
-    queryKey: ["get-user"], // unique cache key
-    queryFn: fetchUser, // the async function
-    staleTime: 0,
-    enabled: false,
-  });
-
-  const handleLogin = () => {
-    setLoading(true);
-    loginMutation.mutateAsync({ identity: mobile, otp: code });
+  const postLogin = async (data: any) => {
+    const response = await axiosInstance.post("https://etekanesh.com/account/login/", data);
+    return response;
   };
 
-  const loginMutation = useMutation({
-    mutationFn: (variables: { identity: string; otp: string; }) => axiosInstance.post(`https://etekanesh.com/account/api/otp/verify/`, variables),
-    onError: (error) => alert(error),
-    onSettled: () => setLoading(false),
-    onSuccess: () => {
-      router.replace("/dashboard");
-      return;
-      refetch().then((response) => {
-        if (response.status === "success") {
-          if (response.data.role === 1) {
-            router.replace("/dashboard");
-          }
+  const postOtp = async (data: any) => {
+    const response = await axiosInstance.post("https://etekanesh.com/account/api/otp/send/", data);
+    return response;
+  };
+
+  const handleSubmit = (e: any) => {
+    if (otpSent) {
+      e.preventDefault();
+      postLogin(formData).then((res) => {
+        if (res) {
+          router.replace("/dashboard");
         }
       });
-    },
-  });
+    } else {
+      e.preventDefault();
+      postOtp(formData).then((res) => {
+        if (res) {
+          setOtpSent(true);
+        }
+      });
+    }
+  };
 
   return (
-    <>
-      <Box flex={1} minHeight={"100vh"} display="flex" justifyContent="center" alignItems={"center"}>
-        <Box>
-          <Box textAlign={"center"}>
-            <Image
-              src={Logo.src}
-              width={100}
-              height={100}
-              alt="بشری"
-              style={{ marginBottom: 40, boxShadow: "0 5px 30px 10px #2873ac96" }}
-            />
-            <Typography variant="h4">پنل هلدینگ بشری</Typography>
-          </Box>
-
-          {step === 0 ? (
-            <Paper sx={{ p: 5, my: 5, borderRadius: 3 }}>
-              <label>شماره موبایل</label>
-              <TextField
-                fullWidth
-                size="small"
-                type="number"
-                margin="dense"
-                sx={{ mb: 3 }}
-                value={mobile}
-                onChange={(e) => setMobile(e.target.value)}
-              />
-              <Button loading={loading} variant="contained" fullWidth sx={{ mt: 5 }} onClick={() => handleVerify()}>
-                درخواست کد تایید
-              </Button>
-            </Paper>
-          ) : step === 1 ? (
-            <Paper sx={{ p: 5, my: 5, borderRadius: 3 }}>
-              <label>لطفا کد پیامک شده را وارد کنید</label>
-              <TextField
-                fullWidth
-                size="small"
-                type="number"
-                margin="dense"
-                sx={{ mb: 3 }}
-                value={code}
-                onChange={(e) => setCode(e.target.value)}
-              />
-              <Button loading={loading} variant="contained" fullWidth sx={{ mt: 5 }} onClick={() => handleLogin()}>
-                ورود
-              </Button>
-              <Button onClick={() => setStep(0)} sx={{ mt: 1 }}>
-                تغییر شماره {mobile}
-              </Button>
-            </Paper>
-          ) : (
-            ""
-          )}
+    <Box height={"100vh"} display={"flex"} alignItems={"center"} justifyContent={"center"}>
+      <Box>
+        <Box textAlign={"center"} mb={5}>
+          <Image
+            src={Logo.src}
+            width={100}
+            height={100}
+            alt="بشری"
+            style={{ marginBottom: 40, boxShadow: "0 5px 30px 10px #2873ac96" }}
+          />
+          <Typography variant="h4">پنل هلدینگ بشری</Typography>
         </Box>
+        <form
+          onSubmit={handleSubmit}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            maxWidth: "300px",
+            gap: "30px",
+          }}
+        >
+          <TextField
+            type="text"
+            name="identity"
+            placeholder="شماره تلفن"
+            value={formData.identity}
+            fullWidth
+            sx={{
+              "& .MuiOutlinedInput-root": {
+                height: "34px",
+                borderRadius: "8px",
+                "& .MuiInputBase-input": {
+                  fontSize: "12px",
+                },
+              },
+            }}
+            onChange={handleChange}
+          />
+          {otpSent && (
+            <TextField
+              type="text"
+              name="otp"
+              placeholder="Enter OTP"
+              value={formData.otp}
+              sx={{
+                "& .MuiOutlinedInput-root": {
+                  height: "34px",
+                  borderRadius: "8px",
+                  "& .MuiInputBase-input": {
+                    fontSize: "12px",
+                  },
+                },
+              }}
+              onChange={handleChange}
+            />
+          )}
+
+          <Button type="submit" variant="contained">
+            ورود
+          </Button>
+        </form>
       </Box>
-    </>
+    </Box>
   );
 }
---
+++ src/app/page000.tsx
@@ -0,0 +1,123 @@
+"use client";
+import { Box, Button, Paper, TextField, Typography } from "@mui/material";
+import Image from "next/image";
+import Logo from "@/assets/logo.jpg";
+import { useRouter } from "next/navigation";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { axiosInstance } from "@/utils/axios";
+import { useState } from "react";
+
+export default function Login() {
+  const router = useRouter();
+  const [mobile, setMobile] = useState("");
+  const [code, setCode] = useState("");
+  const [step, setStep] = useState(0);
+  const [loading, setLoading] = useState(false);
+
+  const handleVerify = () => {
+    setLoading(true);
+    otpRequestMutation.mutateAsync({ identity: mobile });
+  };
+
+  const otpRequestMutation = useMutation({
+    mutationFn: (variables:{ identity: string; }) => axiosInstance.post(`https://etekanesh.com/account/api/otp/send/`, variables),
+    onError: (error) => alert(error),
+    onSettled: () => setLoading(false),
+    onSuccess: () => setStep(1),
+  });
+
+  const fetchUser = async () => {
+    setLoading(true);
+    const { data } = await axiosInstance.get(`https://etekanesh.com/api/account/detail/`);
+    setLoading(false);
+    return data;
+  };
+
+  // Use the tanstack composable
+  const { refetch } = useQuery({
+    queryKey: ["get-user"], // unique cache key
+    queryFn: fetchUser, // the async function
+    staleTime: 0,
+    enabled: false,
+  });
+
+  const handleLogin = () => {
+    setLoading(true);
+    loginMutation.mutateAsync({ identity: mobile, otp: code });
+  };
+
+  const loginMutation = useMutation({
+    mutationFn: (variables: { identity: string; otp: string; }) => axiosInstance.post(`https://etekanesh.com/account/api/otp/verify/`, variables),
+    onError: (error) => alert(error),
+    onSettled: () => setLoading(false),
+    onSuccess: () => {
+      router.replace("/dashboard");
+      return;
+      refetch().then((response) => {
+        if (response.status === "success") {
+          if (response.data.role === 1) {
+            router.replace("/dashboard");
+          }
+        }
+      });
+    },
+  });
+
+  return (
+    <>
+      <Box flex={1} minHeight={"100vh"} display="flex" justifyContent="center" alignItems={"center"}>
+        <Box>
+          <Box textAlign={"center"}>
+            <Image
+              src={Logo.src}
+              width={100}
+              height={100}
+              alt="بشری"
+              style={{ marginBottom: 40, boxShadow: "0 5px 30px 10px #2873ac96" }}
+            />
+            <Typography variant="h4">پنل هلدینگ بشری</Typography>
+          </Box>
+
+          {step === 0 ? (
+            <Paper sx={{ p: 5, my: 5, borderRadius: 3 }}>
+              <label>شماره موبایل</label>
+              <TextField
+                fullWidth
+                size="small"
+                type="number"
+                margin="dense"
+                sx={{ mb: 3 }}
+                value={mobile}
+                onChange={(e) => setMobile(e.target.value)}
+              />
+              <Button loading={loading} variant="contained" fullWidth sx={{ mt: 5 }} onClick={() => handleVerify()}>
+                درخواست کد تایید
+              </Button>
+            </Paper>
+          ) : step === 1 ? (
+            <Paper sx={{ p: 5, my: 5, borderRadius: 3 }}>
+              <label>لطفا کد پیامک شده را وارد کنید</label>
+              <TextField
+                fullWidth
+                size="small"
+                type="number"
+                margin="dense"
+                sx={{ mb: 3 }}
+                value={code}
+                onChange={(e) => setCode(e.target.value)}
+              />
+              <Button loading={loading} variant="contained" fullWidth sx={{ mt: 5 }} onClick={() => handleLogin()}>
+                ورود
+              </Button>
+              <Button onClick={() => setStep(0)} sx={{ mt: 1 }}>
+                تغییر شماره {mobile}
+              </Button>
+            </Paper>
+          ) : (
+            ""
+          )}
+        </Box>
+      </Box>
+    </>
+  );
+}
